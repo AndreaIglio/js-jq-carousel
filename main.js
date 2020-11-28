47b5c5e3ff2519ab6c1d93c571f0ccdca7e1ad7f
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-28 17:57:57 +0100

    Completed Project

--- main.js
+++ main.js
@@ -7,38 +7,55 @@
 
 $(function (){
 
+
 //variabili definite
 
-var next = $('i.fa-angle-right');
-var prev = $('i.fa-angle-left');
+var next = $("i.fa-angle-right");
+var prev = $("i.fa-angle-left");
 
 
+next.click(function (){
 
+    var active = $(".active");
+    var last = $(".last");
+    var first = $(".first");
+    
+    
+    if(active.hasClass('last')){
 
-next.click(function ( ){
-
+     first.addClass('active');
+     last.removeClass('active');
+    }
+    else {
+         $(".active").removeClass("active").next().addClass("active");
+    }
+    
+    
    
-    $(".active").removeClass("active").next().addClass("active");
-
-   
-
 } )
 
 
 
 prev.click(function (){
 
-
-
-    $(".active").removeClass("active").prev().addClass("active");
-    // if($(".active, .first))
-        
+    var active = $(".active");
+    var last = $(".last");
+    var first = $(".first");
     
+
+    if (active.hasClass("first")) {
+     
+     last.addClass("active");
+     first.removeClass("active");
+    }
+    else {
+        active.removeClass("active").prev().addClass("active");
+
+    }
 
 
 
 })
-
 
 
 
